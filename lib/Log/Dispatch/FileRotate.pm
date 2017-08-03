86dde20f25305ccc195c47ca040b6ee178607841
Michael Schout <mschout@gkg.net>
2017-08-03 15:42:11 -0500

    Build results of cdd5e35 (on master)

--- lib/Log/Dispatch/FileRotate.pm
+++ lib/Log/Dispatch/FileRotate.pm
@@ -1,5 +1,5 @@
 package Log::Dispatch::FileRotate;
-$Log::Dispatch::FileRotate::VERSION = '1.32';
+$Log::Dispatch::FileRotate::VERSION = '1.33';
 # ABSTRACT: Log to Files that Archive/Rotate Themselves
 
 require 5.005;
@@ -682,7 +682,7 @@ Log::Dispatch::FileRotate - Log to Files that Archive/Rotate Themselves
 
 =head1 VERSION
 
-version 1.32
+version 1.33
 
 =head1 SYNOPSIS
 
