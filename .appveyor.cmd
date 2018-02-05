87fc8dc3ff98f9935a879714b62b1917d07c9617
Zak B. Elep <zakame@zakame.net>
2018-02-05 21:20:45 +0800

    Hook dzil setup into AppVeyor perl_setup

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -27,6 +27,7 @@ if "%perl_type%" == "cygwin" (
 for /f "usebackq delims=" %%d in (`perl -MConfig -e"print $Config{make}"`) do set make=%%d
 set "perl=perl"
 set "cpanm=call .appveyor.cmd cpanm"
+set "dzil=call .appveyor.cmd dzil"
 set "cpan=%perl% -S cpan"
 set TAR_OPTIONS=--warning=no-unknown-keyword
 goto :eof
