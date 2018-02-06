1070cdcc5e15a3d5a1a69d114b9acab2a6312271
Zak B. Elep <zakame@zakame.net>
2018-02-07 02:23:57 +0800

    Add openssl-devel to cygwin

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -6,7 +6,7 @@ goto :eof
 if not exist "c:\tmp" mkdir c:\tmp
 if not defined perl_type set perl_type=system
 if "%perl_type%" == "cygwin" (
-  start /wait c:\cygwin\setup-x86.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
+  start /wait c:\cygwin\setup-x86.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel -P openssl-devel
   set "PATH=C:\cygwin\usr\local\bin;C:\cygwin\bin;%PATH%"
   set "PERL_MM_OPT=CCFLAGS='-fno-stack-protector -I.' CXXFLAGS='-fno-stack-protector -I.'"
   set "PERL_MB_OPT=CFLAGS='-fno-stack-protector -I.'"
